bd8cfadcb9a5a8f31a822bcb6feefc0904acb5dd
AdamP <adamp959@hotmail.com>
2022-08-23 21:44:03 +0100

    Starter zone drops fixed/reward system fixed (placeholders)

--- azura-server/src/main/java/com/ruse/world/content/progressionzone/ZoneData.java
+++ azura-server/src/main/java/com/ruse/world/content/progressionzone/ZoneData.java
@@ -10,12 +10,12 @@ public class ZoneData {
 
     public enum Monsters {
 
-        SONIC(9001, 10, new Item[]{new Item(19944), new Item(19945), new Item(19946)}),
-        PATRICK(9002, 25, new Item[]{new Item(703), new Item(704), new Item(705)}),
-        LUIGI(9003, 50, new Item[]{new Item(14924), new Item(14919), new Item(14915)}),
-        SQUIRTLE(9004, 100, new Item[]{new Item(ItemDefinition.TOKEN_ID, 1000),  new Item(2734, 1),  new Item(19114, 5)}),
-        MEWTWO(9005, 175, new Item[]{new Item(23044),  new Item(ItemDefinition.TOKEN_ID, 5000),  new Item(2734, 5)}),
-        BOWSER(9006, 225, new Item[]{new Item(ItemDefinition.COIN_ID, 10000000)})
+        SONIC(9001, 10, new Item[]{new Item(995, 69), new Item(995, 69), new Item(995, 69)}),
+        PATRICK(9002, 25, new Item[]{new Item(995, 69), new Item(995, 69), new Item(995, 69)}),
+        LUIGI(9003, 50, new Item[]{new Item(995, 69), new Item(995, 69), new Item(995, 69)}),
+        SQUIRTLE(9004, 100, new Item[]{new Item(995, 69), new Item(995, 69), new Item(995, 69)}),
+        MEWTWO(9005, 175, new Item[]{new Item(995, 69), new Item(995, 69), new Item(995, 69)}),
+        BOWSER(9006, 225, new Item[]{new Item(995, 69), new Item(995, 69), new Item(995, 69)}),
         ;
 
         @Getter
