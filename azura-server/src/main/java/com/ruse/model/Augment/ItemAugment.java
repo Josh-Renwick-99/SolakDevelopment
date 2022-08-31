4611cf54023da6f8d87a24142b405b72676349bc
joshua.renwick <joshua.renwick@boohooo.com>
2022-08-31 20:38:04 +0100

    Damage and DDR bonuses applied

--- azura-server/src/main/java/com/ruse/model/Augment/ItemAugment.java
+++ azura-server/src/main/java/com/ruse/model/Augment/ItemAugment.java
@@ -12,6 +12,62 @@ public class ItemAugment {
     private Augment secondAugment;
     private Augment thirdAugment;
 
+    public double getDamageBonus(){
+        double bonus = 0.0;
+        if (firstAugment != null){
+            bonus += firstAugment.getAugmentBonus(AugmentBonusType.DAMAGE);
+        }
+        if (secondAugment != null){
+            bonus += secondAugment.getAugmentBonus(AugmentBonusType.DAMAGE);
+        }
+        if(thirdAugment != null) {
+            thirdAugment.getAugmentBonus(AugmentBonusType.DAMAGE);
+        }
+        return bonus;
+    }
+
+    public double getHpBonus(){
+        double bonus = 0.0;
+        if (firstAugment != null){
+            bonus += firstAugment.getAugmentBonus(AugmentBonusType.HP);
+        }
+        if (secondAugment != null){
+            bonus += secondAugment.getAugmentBonus(AugmentBonusType.HP);
+        }
+        if(thirdAugment != null) {
+            thirdAugment.getAugmentBonus(AugmentBonusType.HP);
+        }
+        return bonus;
+    }
+
+    public double getDrBonus(){
+        double bonus = 0.0;
+        if (firstAugment != null){
+            bonus += firstAugment.getAugmentBonus(AugmentBonusType.DR);
+        }
+        if (secondAugment != null){
+            bonus += secondAugment.getAugmentBonus(AugmentBonusType.DR);
+        }
+        if(thirdAugment != null) {
+            thirdAugment.getAugmentBonus(AugmentBonusType.DR);
+        }
+        return bonus;
+    }
+
+    public double getDdrBonus(){
+        double bonus = 0.0;
+        if (firstAugment != null){
+            bonus += firstAugment.getAugmentBonus(AugmentBonusType.DDR);
+        }
+        if (secondAugment != null){
+            bonus += secondAugment.getAugmentBonus(AugmentBonusType.DDR);
+        }
+        if(thirdAugment != null) {
+            thirdAugment.getAugmentBonus(AugmentBonusType.DDR);
+        }
+        return bonus;
+    }
+
     public enum AugmentType {
         WEAPON, ARMOUR;
     }
